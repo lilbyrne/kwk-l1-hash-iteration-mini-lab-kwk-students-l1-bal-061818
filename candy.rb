65c0a1d423b85229faeaa747590be1d604784fe0
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-22 10:42:43 +0000

    automatically backed up by learn

--- candy.rb
+++ candy.rb
@@ -1,17 +1,39 @@
-#assign everyone's favorite candy to their name
-# how to combine two arrays into one hash 
+
+scholars = ["Avani", "Grace", "Siobhan", "Patch", "Anu", "Cassidy", "Jesse", "Seema", "Shay", "Ume", "Irha", "Ava", "Lily", "Hadje"]
+candy = ["skittles", "hersheys", "reeses", "lolipops", "almond_joy", "twix", "fun_dip", "twix", "twix","twizzlers","dark_chocolate","kit_kat","sour_patch","twix"]
+
+fav_candy={}
+
+each_candy = 6 
+scholars.each do |scandy|
+  fav_candy[scandy] = candy[each_candy]
+  each_candy += 1 
+if each_candy == candy.length
+  each_candy = 0 
+end 
+end 
+puts fav_candy 
+
+
+
+#Growing a hash with two arrays. You use array.each (how to combine two arrays into one hash) In this example we want to combine the student's name with their favorite candy)
 
 scholars = ["Avani", "Grace", "Siobhan", "Patch", "Anu", "Cassidy",]
 candy = [ "skittles", "hersheys", "reeses", "lolipops", "twix", "twix",]
 
+
+#you have two arrays, how do you combine them into a hash called fav_candy?
 fav_candy={}
-#this means you are creating a hash
 
+#next you have to pick a name for your list of candys so you can tell the list to do thing. Once you do this, you have to say where this new list starts, assign a your starting value.
 each_candy = 0 
-#this is your starting point start with your first value and assign each key to a . each_candy is the list of all candys
 
+#okay now you have to rename scholars so you can call them all something 
 scholars.each do |scandy|
-  fav_candy[scandy] = candy [each_candy]
+  
+#so now you have to call all of the scholars out of the hash. The hash is called fav_candy and the list of students is scandy. Then you want to pull out the matching individual candy out of the list of candies. 
+  fav_candy[scandy] = candy[each_candy]
+  
   each_candy += 1 
   #this is teling each_candy to go on to the next value in the array and assign it to the next value in scandy
 end 
